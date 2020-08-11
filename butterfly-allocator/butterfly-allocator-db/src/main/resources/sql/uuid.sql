9e82e02b2555444b363a235c8424b455e214a1a2
simonalong <simonalong@126.com>
2020-08-11 18:39:04 +0800

    chg 修改sql的字段

--- butterfly-allocator/butterfly-allocator-db/src/main/resources/sql/uuid.sql
+++ butterfly-allocator/butterfly-allocator-db/src/main/resources/sql/uuid.sql
@@ -4,7 +4,7 @@ CREATE TABLE `butterfly_uuid_generator` (
   `work_id` int(16) COMMENT '工作id',
   `last_expire_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '下次失效时间',
   `uid` varchar(128) DEFAULT '0' COMMENT '本次启动唯一id',
-  `ip` bigint(20) NOT NULL DEFAULT '0' COMMENT 'ip',
+  `ip` varchar(20) NOT NULL DEFAULT '0' COMMENT 'ip',
   `process_id` varchar(128) NOT NULL DEFAULT '0' COMMENT '进程id',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_name_work` (`namespace`,`work_id`)
